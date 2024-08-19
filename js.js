a2c2fcf40862cf1625e02d76d1f2465495a5f971
vansh-frontend <vanshdhalor04@gmail.com>
2024-08-19 20:24:02 +0530

    init

--- js.js
+++ js.js
@@ -40,3 +40,13 @@ const cards = [
         price: "$21.00"
     }
 ];
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navMenu = document.getElementById('nav-menu');
+  
+    menuIcon.addEventListener('click', function() {
+      navMenu.classList.toggle('active');
+    });
+  });
+  
